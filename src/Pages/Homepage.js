3f70b2b5612a6c2f3ce012216cd1f67fa815676d
Belle Pan <belle.pan@mail.mcgill.ca>
2021-09-11 18:46:18 -0400

    adjusted spacing

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -13,87 +13,85 @@ import Container from "@material-ui/core/Container";
 import Search from "../Components/Search";
 
 const useStyles = makeStyles((theme) => ({
-    "@global": {
-        ul: {
-            margin: 0,
-            padding: 0,
-            listStyle: "none",
-        },
-    },
-    link: {
-        margin: theme.spacing(1, 1.5),
-    },
-    heroContent: {
-        padding: theme.spacing(8, 0, 6),
-    },
-    cardHeader: {
-        backgroundColor:
-            theme.palette.type === "light"
-                ? theme.palette.grey[200]
-                : theme.palette.grey[700],
-    },
-    // cardPricing: {
-    // 	display: "flex",
-    // 	justifyContent: "center",
-    // 	alignItems: "baseline",
-    // 	marginBottom: theme.spacing(2),
-    // },
+	"@global": {
+		ul: {
+			margin: 0,
+			padding: 0,
+			listStyle: "none",
+		},
+	},
+	link: {
+		margin: theme.spacing(1, 1.5),
+	},
+	heroContent: {
+		padding: theme.spacing(8, 0, 6),
+	},
+	cardHeader: {
+		backgroundColor:
+			theme.palette.type === "light"
+				? theme.palette.grey[200]
+				: theme.palette.grey[700],
+	},
+	// cardPricing: {
+	// 	display: "flex",
+	// 	justifyContent: "center",
+	// 	alignItems: "baseline",
+	// 	marginBottom: theme.spacing(2),
+	// },
 }));
 
 export default function Homepage() {
-    const classes = useStyles();
-    const [titles, setTitles] = useState({});
+	const classes = useStyles();
+	const [titles, setTitles] = useState({});
 
-    // useEffect(() => {
-    // 	fetch('/recommendations').then(
-    // 		res => res.json()
-    // 	).then(
-    // 		data => {
-    // 			setData(data);
-    // 		}
-    // 	);
-    // }, []);
+	// useEffect(() => {
+	// 	fetch('/recommendations').then(
+	// 		res => res.json()
+	// 	).then(
+	// 		data => {
+	// 			setData(data);
+	// 		}
+	// 	);
+	// }, []);
 
-    useEffect(() => {
-        fetch('/getAllTitles').then(
-            res => res.json()
-        ).then(
-            titles => {
-                setTitles(titles)
-            }
-        );
-    }, []);
+	useEffect(() => {
+		fetch("/getAllTitles")
+			.then((res) => res.json())
+			.then((titles) => {
+				setTitles(titles);
+			});
+	}, []);
 
-    return (
-        <div>
-            <React.Fragment>
-                {/*The title and subtitle of the page aka HERO CONTENT*/}
-                <Container
-                    maxWidth="sm"
-                    component="main"
-                    className={classes.heroContent}
-                >
-                    <Typography
-                        component="h1"
-                        variant="h2"
-                        align="center"
-                        color="textPrimary"
-                        gutterBottom
-                    >
-                        Book Recommendation
+	return (
+		<div>
+			<React.Fragment>
+				{/*The title and subtitle of the page aka HERO CONTENT*/}
+				<Container
+					maxWidth="sm"
+					component="main"
+					className={classes.heroContent}
+				>
+					<Typography
+						component="h1"
+						variant="h2"
+						align="center"
+						color="textPrimary"
+						gutterBottom
+					>
+						Book Recommendation
 					</Typography>
-                    <Typography
-                        variant="h5"
-                        align="center"
-                        color="textSecondary"
-                        component="p"
-                    >
-                        Find your next read below! What is a book you enjoyed?
+					<Typography
+						variant="h5"
+						align="center"
+						color="textSecondary"
+						component="p"
+					>
+						Find your next read below! What is a book you enjoyed?
 					</Typography>
-                    <Search options={titles.titles} />
-                </Container>
-
-            </React.Fragment>
-        </div>
-    );
+					<h3 />
+					<Search options={titles.titles} />
+				</Container>
+			</React.Fragment>
+		</div>
+	);
 }
