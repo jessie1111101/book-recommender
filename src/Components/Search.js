43cc20b26052fa69945e284c002a5e41668ce7e8
Jessie Liu <58928222+jessie1111101@users.noreply.github.com>
2021-09-11 18:49:43 -0400

    disabled clear on select

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -4,55 +4,56 @@ import { Autocomplete } from "@material-ui/lab";
 import { TextField, Button } from "@material-ui/core";
 
 class Search extends Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = {
-			selected: "",
+        this.state = {
+            selected: "",
 
-			displayError: false,
-		};
-	}
+            displayError: false,
+        };
+    }
 
-	setSelected = (title) => this.setState({ selected: title });
+    setSelected = (title) => this.setState({ selected: title });
 
-	handleOnClick = () => {
-		if (this.state.selected == "") {
-			this.setState({ displayError: true });
-		} else {
-			this.setState({ displayError: false });
-			console.log("router display results " + this.state.selected);
-		}
-	};
+    handleOnClick = () => {
+        if (this.state.selected == "") {
+            this.setState({ displayError: true });
+        } else {
+            this.setState({ displayError: false });
+            console.log("router display results " + this.state.selected);
+        }
+    };
 
-	render() {
-		let { options } = this.props;
-		let { displayError } = this.state;
+    render() {
+        let { options } = this.props;
+        let { displayError } = this.state;
 
-		return (
-			<>
-				<Autocomplete
-					id="search-box"
-					options={options}
-					style={{ width: 600 }}
-					renderInput={(params) => (
-						<TextField
-							{...params}
-							label="Enter a book title"
-							variant="outlined"
-						/>
-					)}
-					onChange={(e) => this.setSelected(e.target.innerHTML)}
-				/>
-				<h3 />
-				<Button variant="contained" onClick={this.handleOnClick}>
-					Get Recommendations
+        return (
+            <>
+                <Autocomplete
+                    id="search-box"
+                    options={options}
+                    style={{ width: 600 }}
+                    disableClearable={true}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label="Enter a book title"
+                            variant="outlined"
+                        />
+                    )}
+                    onChange={(e) => this.setSelected(e.target.innerHTML)}
+                />
+                <h3 />
+                <Button variant="contained" onClick={this.handleOnClick}>
+                    Get Recommendations
 				</Button>
-				{displayError ? (
-					<div className="red-text">Please select a book</div>
-				) : null}
-			</>
-		);
-	}
+                {displayError ? (
+                    <div className="red-text">Please select a book</div>
+                ) : null}
+            </>
+        );
+    }
 }
 export default Search;
