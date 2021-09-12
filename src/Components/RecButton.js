86faf4e5b1ad0765b4857ff157107137f20530ad
Belle Pan <belle.pan@mail.mcgill.ca>
2021-09-11 20:00:09 -0400

    changed alignment of recommendation button

--- src/Components/RecButton.js
+++ src/Components/RecButton.js
@@ -1,24 +1,38 @@
 import React, { Component } from "react";
 
-import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Box, Button } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 
-class RecButton extends Component {
-	render() {
-		return (
-			<>
+const style = makeStyles({
+	titleItemRight: {
+		color: "white",
+		top: "50%",
+		height: 30,
+		float: "right",
+		position: "relative",
+		transform: "translateY(10%)",
+	},
+});
+
+const RecButton = () => {
+	const classes = style();
+	return (
+		<div>
+			<Box className={classes.titleBar}>
 				<Button
+					className={classes.titleItemRight}
 					variant="contained"
-					onClick={this.handleOnClick}
+					onClick={Component.handleOnClick}
 					color="primary"
 					size="small"
 					endIcon={<SearchIcon />}
 				>
 					Get Recommendations
 				</Button>
-			</>
-		);
-	}
-}
+			</Box>
+		</div>
+	);
+};
 
 export default RecButton;
