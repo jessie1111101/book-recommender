8c9ca8f278a8d926fdf6c89a086512c55e78557f
Jessie Liu <58928222+jessie1111101@users.noreply.github.com>
2021-09-12 03:43:40 -0400

    updated requirements

--- api/api.py
+++ api/api.py
@@ -15,38 +15,31 @@ app = flask.Flask(__name__)
 TfidVectorizer = sklearn.feature_extraction.text.TfidfVectorizer
 linear_kernel = sklearn.metrics.pairwise.linear_kernel
 
-book_csv_url = 'https://raw.githubusercontent.com/zygmuntz/goodbooks-10k/master/books.csv'
-#df_books = pd.read_csv(book_csv_url)
 df_books = pd.read_csv('../goodreads_10k/books.csv')
 books_df = df_books[['book_id', 'original_title', 'authors']]
 books_df = books_df.sort_values('book_id')
 
-book_tags_url = 'https://github.com/zygmuntz/goodbooks-10k/blob/master/book_tags.csv?raw=true'
-#book_tags = pd.read_csv(book_tags_url)
 book_tags = pd.read_csv('../goodreads_10k/book_tags.csv')
 
-tags_url = 'https://raw.githubusercontent.com/zygmuntz/goodbooks-10k/master/tags.csv'
-#tags = pd.read_csv(tags_url)
 tags = pd.read_csv('../goodreads_10k/tags.csv')
 
 merge_tags = pd.merge(book_tags, tags, left_on='tag_id', right_on='tag_id', how='inner')
 
-merge_tags
 
 book_tags = pd.merge(df_books, merge_tags, left_on='book_id',
                      right_on='goodreads_book_id', how='inner')
 
 auth_book_tags = book_tags.groupby('book_id')['tag_name'].apply(' '.join).reset_index()
-auth_book_tags
+
 
 df_books = pd.merge(df_books, auth_book_tags,
                    left_on='book_id', right_on='book_id', how='inner')
-df_books.head()
+
 
 df_books['combined'] = pd.Series(df_books[['authors','tag_name']]
                                 .fillna('').values.tolist()).str.join(' ')
 
-df_books['combined']
+
 
 tf_combined = TfidVectorizer(analyzer='word',ngram_range=(1, 2)
                               ,min_df=0, stop_words='english')
